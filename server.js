7ab800fd5eea9d67aef37e7d956448d1e3c67b69
xavierchen <info@xavchen.com>
2024-10-07 20:05:19 +0100

    added models/fruit.js

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+const dotenv = require('dotenv');
+dotenv.config()
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on('connected', () => {
+    console.log(`connected to MongoDB ${mongoose.connection.name}.`)
+})
 
 app.get('/', (req,res) => {
     res.render('index.ejs')
